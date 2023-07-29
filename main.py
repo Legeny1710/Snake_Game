0183d935c7baa4b565bf0178fd6229765e6627ea
Yusuf Kula <yvk2045@gmail.com>
2023-07-29 20:17:04 +0100

    updated the title of the game

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from score_board import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.title("My Snake Game")
+screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
